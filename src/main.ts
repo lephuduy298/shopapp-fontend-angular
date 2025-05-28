ab0add011df5e369d8be4f50c5450fd6005c2d3a
lephuduy298 <leduyphucat@gmail.com>
2025-05-29 01:24:54 +0700

    update basic home page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component';
+// import { AppComponent } from './app/app.component';
+// HomeComponent
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(HomeComponent, appConfig)
   .catch((err) => console.error(err));
