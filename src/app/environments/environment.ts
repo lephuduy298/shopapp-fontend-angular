65b62d2943c3284c8c26f1164123d254de28054c
lephuduy298 <leduyphucat@gmail.com>
2025-09-10 22:50:15 +0700

    docker

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,10 @@
+// export const environment = {
+//     production: false,
+//     apiBaseUrl: 'http://localhost:8080/api/v1',
+// };
+// environment.prod.ts
 export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:8080/api/v1',
+    production: true,
+    apiBaseUrl: '/api/v1',
 };
+
