d86f1dd96db19a20ce9fffa05f35251519d4be75
lephuduy298 <leduyphucat@gmail.com>
2025-09-13 00:11:22 +0700

    redis

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,10 +1,10 @@
-// export const environment = {
-//     production: false,
-//     apiBaseUrl: 'http://localhost:8080/api/v1',
-// };
-// environment.prod.ts
 export const environment = {
-    production: true,
-    apiBaseUrl: '/api/v1',
+    production: false,
+    apiBaseUrl: 'http://localhost:8080/api/v1',
 };
+// environment.prod.ts
+// export const environment = {
+//     production: true,
+//     apiBaseUrl: '/api/v1',
+// };
 
